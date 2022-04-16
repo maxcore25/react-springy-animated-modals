5bc096c507a98c06b901d3a1d8a9f5c59d564ec5
maxcore25 <mkorobov2002@mail.ru>
2022-04-16 09:04:24 +0300

    Logger And Notifications Generator Added

---
+++ src/arr-utils.js
@@ -0,0 +1,14 @@
+export const remove = (arr, item) => {
+  const newArr = [...arr];
+  newArr.splice(
+    newArr.findIndex(i => i === item),
+    1
+  );
+  return newArr;
+};
+
+let newIndex = 0;
+export const add = (arr, text, style) => {
+  newIndex = newIndex + 1;
+  return [...arr, { id: newIndex, text: text, style: style }];
+};
